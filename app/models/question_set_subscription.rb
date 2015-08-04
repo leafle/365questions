67a8e655daf2e76d34ee13e31abbb14aa620faf9
Brandon Smyth <bsmyth77@gmail.com>
2015-08-03 21:57:05 -0700

    Add QuestionSetSubscription.due scope

--- app/models/question_set_subscription.rb
+++ app/models/question_set_subscription.rb
@@ -1,4 +1,6 @@
 class QuestionSetSubscription < ActiveRecord::Base
   belongs_to :question_set
   belongs_to :user
+
+  scope :due, -> { where("(last_ask_at + make_interval(secs := period)) <= NOW()") }
 end
