3950e7a99522adf1eb91d742a02349c8fe0bd713
Brandon Smyth <bsmyth77@gmail.com>
2016-03-05 14:47:00 -0800

    Drop the widgets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803053243) do
+ActiveRecord::Schema.define(version: 20160305223601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,14 +77,6 @@ ActiveRecord::Schema.define(version: 20150803053243) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
